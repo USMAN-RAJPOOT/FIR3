b6137f986884d1ca9a840b987e3ab17e49a9085b
USMAN-RAJPOOT <113978329+USMAN-RAJPOOT@users.noreply.github.com>
2023-01-06 23:01:48 +0500

    Update USMAN.py

--- USMAN.py
+++ USMAN.py
@@ -16,7 +16,7 @@ if bit == '64bit':
 
 elif bit == '32bit':
     print("\033[1;32m[•] \033[1;33mCONGRATULATIONS YOUR DEVICE SUPPORT THE  TOOL\033[1;35m ")
-    from MANI64 import Main
+    from MANI32 import Main
     Main()
 
        
