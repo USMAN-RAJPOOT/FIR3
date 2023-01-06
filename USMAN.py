f3b32d84c34349ff67fc50239a0b67f7c783f48a
USMAN-RAJPOOT <113978329+USMAN-RAJPOOT@users.noreply.github.com>
2023-01-06 22:52:15 +0500

    Update USMAN.py

--- USMAN.py
+++ USMAN.py
@@ -15,7 +15,10 @@ if bit == '64bit':
     Main()
 
 elif bit == '32bit':
+    print("\033[1;32m[•] \033[1;33mCONGRATULATIONS YOUR DEVICE SUPPORT THE  TOOL\033[1;35m ")
+    from MANI64 import Main
+    Main()
 
-      print("\033[1;32m[•] \033[1;33mWAIT 32BIT COMMING SOON IN FEW TIME")
+       
 
-      os.system('exit')
+      
